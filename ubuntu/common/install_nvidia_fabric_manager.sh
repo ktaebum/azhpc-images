dfad4b3f2f82ba9f71872296f792398413425f1d
Taebum Kim <phya.ktaebum@gmail.com>
2023-04-21 19:32:06 +0900

    downgrade

--- ubuntu/common/install_nvidia_fabric_manager.sh
+++ ubuntu/common/install_nvidia_fabric_manager.sh
@@ -6,11 +6,11 @@ set -ex
 VERSION=$1
 
 # Install nvidia fabric manager (required for ND96asr_v4)
-NVIDIA_FABRIC_MANAGER_VERSION="510_510.47.03-1"
+NVIDIA_FABRIC_MANAGER_VERSION="470_470.57.02-1"
 NVIDIA_FABRIC_MNGR_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "5636b87336c3ba0533c9ad7e85e7e5545e446056d08b1dbd63b7ca65c4f8f750"
+# $COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "5636b87336c3ba0533c9ad7e85e7e5545e446056d08b1dbd63b7ca65c4f8f750"
 apt install -y ./nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-sudo apt-mark hold nvidia-fabricmanager-510
+sudo apt-mark hold nvidia-fabricmanager-470
 $COMMON_DIR/write_component_version.sh "NVIDIA_FABRIC_MANAGER" ${NVIDIA_FABRIC_MANAGER_VERSION}
 systemctl enable nvidia-fabricmanager
 systemctl start nvidia-fabricmanager
--- ubuntu/common/install_nvidiagpudriver.sh
+++ ubuntu/common/install_nvidiagpudriver.sh
@@ -2,10 +2,10 @@
 set -ex
 
 # Install Cuda
-NVIDIA_VERSION="510.47.03"
-CUDA_VERSION="11.6.1"
+NVIDIA_VERSION="470.57.02"
+CUDA_VERSION="11.4.1"
 CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/local_installers/cuda_${CUDA_VERSION}_${NVIDIA_VERSION}_linux.run
-$COMMON_DIR/download_and_verify.sh $CUDA_URL "ab219afce00b74200113269866fbff75ead037bcfc23551a8338c2684c984d7e"
+# $COMMON_DIR/download_and_verify.sh $CUDA_URL "ab219afce00b74200113269866fbff75ead037bcfc23551a8338c2684c984d7e"
 chmod +x cuda_${CUDA_VERSION}_${NVIDIA_VERSION}_linux.run
 sh cuda_${CUDA_VERSION}_${NVIDIA_VERSION}_linux.run --silent
 echo 'export PATH=$PATH:/usr/local/cuda/bin' | tee -a /etc/bash.bashrc
@@ -13,18 +13,18 @@ echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64' | tee -a /e
 $COMMON_DIR/write_component_version.sh "CUDA" ${CUDA_VERSION}
 
 # Download CUDA samples
-CUDA_SAMPLES_VERSION="11.6"
+CUDA_SAMPLES_VERSION="11.4"
 TARBALL="v${CUDA_SAMPLES_VERSION}.tar.gz"
 CUDA_SAMPLES_DOWNLOAD_URL=https://github.com/NVIDIA/cuda-samples/archive/refs/tags/${TARBALL}
 wget ${CUDA_SAMPLES_DOWNLOAD_URL}
 tar -xvf ${TARBALL}
 pushd ./cuda-samples-${CUDA_SAMPLES_VERSION}
 make
-cp -r ./Samples/* /usr/local/cuda-11.6/samples/
+cp -r ./Samples/* /usr/local/cuda-11.4/samples/
 popd
 
 # Nvidia driver
 NVIDIA_DRIVER_URL=https://download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_VERSION}.run
-$COMMON_DIR/download_and_verify.sh $NVIDIA_DRIVER_URL "f2a421dae836318d3c0d96459ccb3af27e90e50c95b0faa4288af76279e5d690"
+# $COMMON_DIR/download_and_verify.sh $NVIDIA_DRIVER_URL "f2a421dae836318d3c0d96459ccb3af27e90e50c95b0faa4288af76279e5d690"
 bash NVIDIA-Linux-x86_64-${NVIDIA_VERSION}.run --silent --dkms
 $COMMON_DIR/write_component_version.sh "NVIDIA" ${NVIDIA_VERSION}
