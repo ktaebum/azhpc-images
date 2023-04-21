879e18dcc0460d18a41327192b5c11fe6d17e299
Taebum Kim <phya.ktaebum@gmail.com>
2023-04-21 19:20:22 +0900

    downgrade

--- ubuntu/common/install_nvidia_fabric_manager.sh
+++ ubuntu/common/install_nvidia_fabric_manager.sh
@@ -6,10 +6,10 @@ set -ex
 VERSION=$1
 
 # Install nvidia fabric manager (required for ND96asr_v4)
-NVIDIA_FABRIC_MANAGER_VERSION="470_470.82.01-1"
+NVIDIA_FABRIC_MANAGER_VERSION="470_470.52.02-1"
 $COMMON_DIR/write_component_version.sh "NVIDIA_FABRIC_MANAGER" ${NVIDIA_FABRIC_MANAGER_VERSION}
 NVIDIA_FABRIC_MNGR_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "f43630169ac30fc0f5ad02727c1ff8f6a829268358bf35ac52e3f2d12a9bf4b2"
+# $COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "f43630169ac30fc0f5ad02727c1ff8f6a829268358bf35ac52e3f2d12a9bf4b2"
 apt install -y ./nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
 sudo apt-mark hold nvidia-fabricmanager-470
 systemctl enable nvidia-fabricmanager
