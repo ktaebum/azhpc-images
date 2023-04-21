77e43a2b17bf13522f9e274b21ab8065c6da6a0d
Taebum Kim <phya.ktaebum@gmail.com>
2023-04-21 20:58:13 +0900

    temp

--- ubuntu/common/install_nvidia_fabric_manager.sh
+++ ubuntu/common/install_nvidia_fabric_manager.sh
@@ -6,9 +6,10 @@ set -ex
 VERSION=$1
 
 # Install nvidia fabric manager
-NVIDIA_FABRIC_MANAGER_VERSION="520_520.61.05-1"
+NVIDIA_FABRIC_MANAGER_VERSION="470.57.02-1"
 NVIDIA_FABRIC_MNGR_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "8beeb76ade836327298337b4f65eb3906498cebe71ab35bca5bb1a638c1bfd0a"
+# $COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "8beeb76ade836327298337b4f65eb3906498cebe71ab35bca5bb1a638c1bfd0a"
+wget $NVIDIA_FABRIC_MNGR_URL
 apt install -y ./nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-sudo apt-mark hold nvidia-fabricmanager-520
+sudo apt-mark hold nvidia-fabricmanager-470
 $COMMON_DIR/write_component_version.sh "NVIDIA_FABRIC_MANAGER" ${NVIDIA_FABRIC_MANAGER_VERSION}
--- ubuntu/common/install_nvidiagpudriver.sh
+++ ubuntu/common/install_nvidiagpudriver.sh
@@ -7,8 +7,8 @@ CHECKSUM=$2
 
 # Reference - https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#ubuntu-installation
 # Install Cuda
-NVIDIA_VERSION="520.61.05"
-if [ ${RELEASE_VERSION} == "1804" ]; then CUDA_VERSION="11.8"; else CUDA_VERSION="11-8"; fi
+NVIDIA_VERSION="470.57.02"
+if [ ${RELEASE_VERSION} == "1804" ]; then CUDA_VERSION="11.4"; else CUDA_VERSION="11-4"; fi
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${RELEASE_VERSION}/x86_64/cuda-keyring_1.0-1_all.deb
 dpkg -i ./cuda-keyring_1.0-1_all.deb
 
@@ -19,15 +19,15 @@ echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64' | tee -a /e
 $COMMON_DIR/write_component_version.sh "CUDA" ${CUDA_VERSION}
 
 # Download CUDA samples
-CUDA_SAMPLES_VERSION="11.8"
-TARBALL="v${CUDA_SAMPLES_VERSION}.tar.gz"
-CUDA_SAMPLES_DOWNLOAD_URL=https://github.com/NVIDIA/cuda-samples/archive/refs/tags/${TARBALL}
-wget ${CUDA_SAMPLES_DOWNLOAD_URL}
-tar -xvf ${TARBALL}
-pushd ./cuda-samples-${CUDA_SAMPLES_VERSION}
-make
-cp -r ./Samples /usr/local/cuda-11.8/samples/
-popd
+# CUDA_SAMPLES_VERSION="11.8"
+# TARBALL="v${CUDA_SAMPLES_VERSION}.tar.gz"
+# CUDA_SAMPLES_DOWNLOAD_URL=https://github.com/NVIDIA/cuda-samples/archive/refs/tags/${TARBALL}
+# wget ${CUDA_SAMPLES_DOWNLOAD_URL}
+# tar -xvf ${TARBALL}
+# pushd ./cuda-samples-${CUDA_SAMPLES_VERSION}
+# make
+# cp -r ./Samples /usr/local/cuda-11.8/samples/
+# popd
 
 # Nvidia driver
 NVIDIA_DRIVER_URL=https://us.download.nvidia.com/tesla/${NVIDIA_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_VERSION}.run
